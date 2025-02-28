a209cc59016a53d27241f25fa958d21e6f82b37f
Daniil Konoval <1.dankon.1@gmail.com>
2025-02-28 16:09:34 +0300

    Revert "Fullpage landing (#1)"
    This reverts commit 569c6696c7e566a3a3b0dbdea5632b76d3babdb1.

--- components/HeroSlide/MainMenu/index.tsx
+++ components/HeroSlide/MainMenu/index.tsx
@@ -1,32 +1,16 @@
-import React, { FC } from "react";
-import { useRouter } from "next/navigation";
+import React from "react";
+import Link from "next/link";
 
 import classes from "./index.module.css";
 
-interface IMainMenu {
-  onCloseMenu: () => void;
-}
-
-const MainMenu: FC<IMainMenu> = ({ onCloseMenu }) => {
-  const router = useRouter();
-
-  const scrollTo = (blockId: string) => {
-    if (location.pathname.includes("locations")) {
-      router.push("/");
-    }
-    const element = document.getElementById(blockId);
-    element?.scrollIntoView({ behavior: "smooth" });
-
-    onCloseMenu();
-  };
-
+const MainMenu = () => {
   return (
     <div className={classes.mainMenu}>
-      <div onClick={() => scrollTo("home")}>Home</div>
-      <div onClick={() => scrollTo("about")}>About us</div>
-      <div onClick={() => scrollTo("locations")}>Locations</div>
-      <div onClick={() => scrollTo("clients")}>Clients</div>
-      <div onClick={() => scrollTo("contacts")}>Contacts</div>
+      <Link href="/">Home</Link>
+      <Link href="/about">About us</Link>
+      <Link href="/locations">Locations</Link>
+      <Link href="/clients">Clients</Link>
+      <Link href="/contacts">Contacts</Link>
     </div>
   );
 };
--- components/Slide/index.tsx
+++ components/Slide/index.tsx
@@ -1,27 +1,15 @@
-import React, { CSSProperties, FC, PropsWithChildren } from "react";
+import React, { FC, PropsWithChildren } from "react";
 
 import classes from "./index.module.css";
 
 interface ISlide extends PropsWithChildren {
-  style?: CSSProperties;
-  isSmall?: boolean;
   isRedSlide?: boolean;
 }
 
-const Slide: FC<ISlide> = ({ style, isSmall, isRedSlide, children }) => {
+const Slide: FC<ISlide> = ({ isRedSlide, children }) => {
   return (
-    <div
-      style={{ minHeight: isSmall ? "50vh" : "100vh", ...style }}
-      className={`${classes.slide} ${isRedSlide ? classes.slideRed : ""}`}
-    >
-      <div
-        style={{
-          minHeight: isSmall ? "50vh" : "100vh",
-        }}
-        className={classes.slideContainer}
-      >
-        {children}
-      </div>
+    <div className={`${classes.slide} ${isRedSlide ? classes.slideRed : ""}`}>
+      {children}
     </div>
   );
 };
