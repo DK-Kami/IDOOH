15c15313f3d7d7636978f80016c199851d4be885
Konoval Daniil <1.dankon.1@gmail.com>
2025-04-09 19:25:57 +0400

    Added LinkedIn link

--- views/contacts/index.tsx
+++ views/contacts/index.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import { Metadata } from "next";
 
@@ -12,15 +13,25 @@ export const metadata: Metadata = {
 };
 
 const ContactsPage = () => {
+  const goTo = (link: string) => {
+    window.open(link, "_blank");
+  };
+
   return (
     <div id="contacts">
       <Slide>
         <div className={classes.contactsContainer}>
           <div className={classes.contactsTitle}>Contacts</div>
-
           <div className={classes.contactsItem}>
             <div className={classes.contactsItemTitle}>Office</div>
-            <div className={classes.contactsItemValue}>
+            <div
+              className={classes.contactsItemValue}
+              onClick={() =>
+                goTo(
+                  "https://www.google.com/maps/search/1301-0165,+floor+13,+The+One+Tower,+Sheik+Zayed+Road,+Barsha+Heights,+TECOM,+Dubai,+UAE/@25.0992687,55.1745348,1021m/data=!3m2!1e3!4b1?entry=ttu&g_ep=EgoyMDI1MDQwNi4wIKXMDSoASAFQAw%3D%3D",
+                )
+              }
+            >
               1301-0165, floor 13, The One Tower, <br />
               Sheik Zayed Road, Barsha Heights, <br />
               TECOM, Dubai, UAE
@@ -28,11 +39,32 @@ const ContactsPage = () => {
           </div>
           <div className={classes.contactsItem}>
             <div className={classes.contactsItemTitle}>Email</div>
-            <div className={classes.contactsItemValue}>faldina@idooh.ae</div>
+            <div
+              className={classes.contactsItemValue}
+              onClick={() => goTo("mailto:faldina@idooh.ae")}
+            >
+              faldina@idooh.ae
+            </div>
           </div>
           <div className={classes.contactsItem}>
             <div className={classes.contactsItemTitle}>Phone</div>
-            <div className={classes.contactsItemValue}>+ 971 581 733 443</div>
+            <div
+              className={classes.contactsItemValue}
+              onClick={() => goTo("tel:971581733443")}
+            >
+              + 971 581 733 443
+            </div>
+          </div>
+          <div className={classes.contactsItem}>
+            <div className={classes.contactsItemTitle}>LinkedIn</div>
+            <div
+              className={classes.contactsItemValue}
+              onClick={() =>
+                goTo("https://www.linkedin.com/company/idooh-advertising/")
+              }
+            >
+              IDOOH
+            </div>
           </div>
 
           <div className={classes.contactsFooter}>
